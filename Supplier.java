1f28757409fedce6813541efeb577d63031b8b26
Brea <brearenee@gmail.com>
2023-11-09 16:26:25 -0700

    updated framework

--- Supplier.java
+++ Supplier.java
@@ -1,36 +1,274 @@
-import java.io.Serializable;
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+import java.io.BufferedOutputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.BufferedInputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+/**
+ * A supplier may be a manufacturer, distributor, or depot.
+ * Supplier attributes include:
+ * name, demand, inventory on hand, maximum storage capacity,
+ * cost per unit stored, and surplus inventory.
+ *
+ * @author CS4050 (design)
+ * @author Dr. Jody Paul (code)
+ * @version 20231108
+ */
+public class Supplier implements java.io.Serializable {
+    /** Serialization version requirement. */
+    private static final long serialVersionUID = 405001L;
+    /** Default file name for serialized object. */
+    private static final String SERIAL_FILENAME = "supplier.ser";
 
-public class Supplier implements Serializable {
-    public int demand() {
-        return 5;
+    /** Default maximum capacity. */
+    public static final int MAX_CAPACITY = Integer.MAX_VALUE;
+
+    /** Name of supplier. */
+    private String name;
+
+    /** Cost of storage at supplier's location (≥ 0). */
+    private int storageCost;
+    
+    /**
+     * Direct demand at this supplier's location.
+     * If direct demand == 0, then is considered a depot.
+     * If direct demand < 0, then is considered a manufacturer.
+     */
+    private int demand;
+
+    /** Inventory on hand (≥ 0). */
+    private int inventory;
+
+    /** Maximum amount of inventory that can be held. */
+    private int maxCapacity;
+    
+    /**
+     * Surplus, defined as (inventory - demand).
+     * If positive, surplus represents the number of
+     * units subject to storage cost.
+     * If negative, surplus represents unrealized
+     * value (units that would otherwise have been sold).
+     * Storing this value is not necessary because the accessor,
+     * surplus(), computes this value dynamically.
+     */
+    private int surplus;
+
+    /**
+     * Construct a supplier using default values.
+     */
+    public Supplier() {
+        this.name = this.toString();
+        this.storageCost = 0;
+        this.demand = 0;
+        this.inventory = 0;
+        this.maxCapacity = MAX_CAPACITY;
+        this.surplus = 0;
     }
-    public String name(){
-        return "test";
+
+    /**
+     * Fully-parameterized supplier constructor.
+     * Note that surplus is automatically calculated as the
+     * difference between inventory and demand.
+     * @param name the name of this supplier
+     * @param cost the storage cost per unit stored
+     * @param demand the expected demand at this distributing supplier
+     * @param inventory the actual inventory at this supplier
+     * @param capacity the maximum capacity at this supplier
+     */
+    public Supplier(String name,
+                    int cost,
+                    int demand,
+                    int inventory,
+                    int capacity) {
+        this.name = name;
+        this.storageCost = cost;
+        this.demand = demand;
+        this.inventory = inventory;
+        this.maxCapacity = capacity;
+        this.surplus = inventory - demand;
     }
-    public int storageCost(){
-        return 1;
+
+    /**
+     * @return this supplier's name
+     */
+    public String name() { return this.name; }
+    
+    /**
+     * @return the cost of storage at this supplier's location
+     */
+    public int storageCost() { return this.storageCost; }
+
+    /**
+     * @return the maximum number of units that can be stored
+     */
+    public int maxCapacity() { return this.maxCapacity; }
+    
+    /**
+     * @return the demand at this supplier's location.
+     */
+    public int demand() { return this.demand; }
+    
+    /**
+     * @return the current inventory at this supplier's location.
+     */
+    public int inventory() { return this.inventory; }
+    
+    /**
+     * @return the surplus inventory at this supplier's location.
+     */
+    public int surplus() {
+        this.surplus = this.inventory - this.demand;
+        return this.surplus;
     }
-    public int maxCapacity(){
-        return 1;
+
+
+    /**
+     * Compares this supplier to the specified object.
+     * The result is <code>true</code> if and only if the argument is
+     * not <code>null</code> and is a Supplier object with the same
+     * name, storage cost, demand, and inventory values.
+     * @param anObject the object to compare with this supplier
+     * @return <code>true</code> if the given object represents a Supplier
+     *         equivalent to this supplier, <code>false</code> otherwise
+     */
+    @Override
+    public boolean equals(final Object anObject) {
+        if ((anObject == null)
+                || (this.getClass() != anObject.getClass())) {
+            return false;
+        }
+        Supplier other = ((Supplier) anObject);
+        return (this.name.equals(other.name)
+                && this.storageCost == other.storageCost
+                && this.demand == other.demand
+                && this.maxCapacity == other.maxCapacity
+                && this.inventory == other.inventory);
     }
-    public int inventory(){
-        return 1;
+
+    /**
+     * Returns a hash code value for this supplier.
+     * @return hash code value for this supplier
+     */
+    @Override
+    public int hashCode() {
+        if (name == null) return 43;
+        return this.name.hashCode();
     }
-    public int surplus(){
-        return 1;
+
+    /**
+     * Save this supplier to a file.
+     * @param filename the name of the file in which to save this supplier;
+     *                 if null, uses default file name
+     * @return <code>true</code> if successful save;
+     *         <code>false</code> otherwise
+     * @throws java.io.IOException if unexpected IO error
+     */
+    public final boolean save(final String filename) throws java.io.IOException {
+        boolean success = true;
+        String supplierFileName = filename;
+        if (supplierFileName == null) {
+            supplierFileName = Supplier.SERIAL_FILENAME;
+        }
+        // Serialize the supplier.
+        try {
+            OutputStream file = new FileOutputStream(supplierFileName);
+            OutputStream buffer = new BufferedOutputStream(file);
+            ObjectOutput output = new ObjectOutputStream(buffer);
+            try {
+                output.writeObject(this);
+            } finally { output.close(); }
+        } catch (IOException ex) {
+            System.err.println("Unsuccessful save. " + ex);
+            throw ex;
+        }
+
+        // Attempt to deserialize the supplier as verification.
+        try {
+            InputStream file = new FileInputStream(supplierFileName);
+            InputStream buffer = new BufferedInputStream(file);
+            ObjectInput input = new ObjectInputStream(buffer);
+            try {
+                @SuppressWarnings("unchecked") // Accommodate type erasure.
+                Supplier restored = (Supplier) input.readObject();
+                // Simple check that deserialized data matches original.
+                if (!this.toString().equals(restored.toString())) {
+                    System.err.println("[1] State restore did not match save!");
+                    success = false;
+                }
+                if (!this.equals(restored)) {
+                    System.err.println("[2] State restore did not match save!");
+                    success = false;
+                }
+            } finally { input.close(); }
+        } catch (ClassNotFoundException ex) {
+            System.err.println(
+                "Unsuccessful deserialization: Class not found. " + ex);
+            success = false;
+        } catch (IOException ex) {
+            System.err.println("Unsuccessful deserialization: " + ex);
+            success = false;
+        }
+        return success;
     }
-    public boolean save(String str){
-        return false;
+
+    /**
+     * Restore this supplier from a file.
+     * <br /><em>Postconditions:</em>
+     * <blockquote>If successful, previous contents of this supplier have
+     * been replaced by the contents of the file.
+     * If unsuccessful, content of the supplier is unchanged.</blockquote>
+     * @param filename the name of the file from which to restore this supplier;
+     *                 if null, uses default file name
+     * @return <code>true</code> if successful restore;
+     *         <code>false</code> otherwise
+     * @throws java.io.IOException if unexpected IO error
+     */
+    public final boolean restore(final String filename) throws
+    java.io.IOException {
+        boolean success = false;
+        String supplierFileName = filename;
+        if (supplierFileName == null) {
+            supplierFileName = Supplier.SERIAL_FILENAME;
+        }
+        Supplier restored = null;
+        try {
+            InputStream file = new FileInputStream(supplierFileName);
+            InputStream buffer = new BufferedInputStream(file);
+            ObjectInput input = new ObjectInputStream(buffer);
+            try {
+                @SuppressWarnings("unchecked") // Accommodate type erasure.
+                Supplier retrieved = (Supplier) input.readObject();
+                restored = retrieved;
+            } finally {
+                input.close();
+                success = true;
+            }
+        } catch (ClassNotFoundException ex) {
+            System.err.println(
+                "Unsuccessful deserialization: Class not found. " + ex);
+            success = false;
+        } catch (IOException ex) {
+            System.err.println("Unsuccessful deserialization: " + ex);
+            throw ex;
+        }
+        if (restored == null) {
+            System.err.println(
+                "Unsuccessful deserialization: restored == null");
+            success = false;
+        } else {
+            this.name = restored.name;
+            this.storageCost = restored.storageCost;
+            this.demand = restored.demand;
+            this.inventory = restored.inventory;
+            this.maxCapacity = restored.maxCapacity;
+            this.surplus = restored.surplus;
+        }
+        return success;
     }
-    public boolean restore(String str){
-        return false;
-    }
-    //overwrite equal - are these two objects equal, not the same object. so its specific to supplier objects. 
-    public boolean equals(Object o){
-        return false;
-    }
-    // and if you overwrite equals, you have to override hash code. 
-    public int hashCode(){
-        return 1;
-    }
+
 }
--- Transporter.java
+++ Transporter.java
@@ -1,41 +1,283 @@
-import java.io.Serializable;
-public class Transporter implements Serializable {
-    public String name(){
-        return "test";
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+import java.io.BufferedOutputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+/**
+ * A transporter provides transportation service between
+ * two suppliers.
+ * Transporter attributes include:
+ * name, source supplier, destination supplier,
+ * cost per unit shipped,
+ * maximum number of units that can be handled,
+ * and their current allocation of units.
+ *
+ * @author CS4050 (design)
+ * @author Dr. Jody Paul (code)
+ * @version 20231108
+ */
+public class Transporter implements java.io.Serializable {
+    /** Serialization version requirement. */
+    private static final long serialVersionUID = 405001L;
+    /** Default file name for serialized object. */
+    private static final String SERIAL_FILENAME = "transporter.ser";
+
+    /** Maximum number of units any transporter may ship. */
+    public static final int MAX_UNITS = Integer.MAX_VALUE;
+
+    /** Name of the transporter (unique). */
+    private String name;
+    
+    /** Beginning location. */
+    private Supplier from;
+    
+    /** End location. */
+    private Supplier to;
+    
+    /** Cost per unit shipped. */
+    private int costPerUnit;
+    
+    /** Number of units allocated to this transporter. */
+    private int allocation;
+    
+    /** Maximum number of units that can be handled. */
+    private int maxCapacity;
+
+    /** Construct a transporter using default values. */
+    public Transporter() {
+        this.name = this.toString();
+        this.from = null;
+        this.to = null;
+        this.costPerUnit = 0;
+        this.allocation = 0;
+        this.maxCapacity = MAX_UNITS;
     }
-    public int allocation(){
-        return 1;
+
+    /**
+     * Fully parameterized Transporter constructor.
+     * @param name the name of this transporter (must be unique)
+     * @param from the source of goods of this transporter
+     * @param to the receiver of this transporter
+     * @param cost the cost per unit charged by this transporter
+     * @param maximumAllocation the maximum number of units this transporter can be allocated
+     * @param allocation the number of units initially allocated to this transporter
+     */
+    public Transporter(String name,
+                       Supplier from,
+                       Supplier to,
+                       int cost,
+                       int maximumAllocation,
+                       int allocation) {
+        this.name = name;
+        this.from = from;
+        this.to = to;
+        this.costPerUnit = cost;
+        this.maxCapacity = maximumAllocation;
+        this.allocation = allocation;
     }
-    public boolean setAllocation(int x){
-        return false;
-        //if allocation is higher than max capacity, it should be false. 
+
+    /**
+     * @return the name of this transporter
+     */
+    public String name() {
+        return this.name;
     }
-    public int costPerUnit(){
-        return 1;
-    }
-    public int maxCapacity(){
-        return 1;
-    }
+    
+    /**
+     * @return the beginning location for shipments
+     */
     public Supplier from() {
-        Supplier supplier = new Supplier();
-        return supplier;
+        return this.from;
     }
+    
+    /**
+     * @return the final location for shipments
+     */
     public Supplier to() {
-        Supplier supplier = new Supplier();
-        return supplier;
+        return this.to;
     }
-    public boolean save(String str){
-        return false;
+    
+    /**
+     * @return the cost of shipping a single unit
+     */
+    public int costPerUnit() {
+        return this.costPerUnit;
     }
-    public boolean restore(String str){
-        return false;
+    
+    /**
+     * @return the maximum number of units this transporter can handle
+     */
+    public int maxCapacity() {
+        return this.maxCapacity;
     }
-    //overwrite equal - are these two objects equal, not the same object. so its specific to supplier objects. 
-    public boolean equals(Object o){
-        return false;
+    
+    /**
+     * @return the current number of units allocated to this transporter
+     */
+    public int allocation() {
+        return this.allocation;
     }
-    // and if you overwrite equals, you have to override hash code. 
-    public int hashCode(){
-        return 1;
+
+    /**
+     * Modify the allocation of this transporter.
+     * @param allocation the new allocation for this transporter
+     */
+    public void setAllocation(int allocation) {
+        this.allocation = allocation;
+    }
+    
+    /**
+     * Compares this transporter to the specified object.
+     * The result is <code>true</code> if and only if the argument is
+     * not <code>null</code> and is a Transporter object with the same
+     * name, from, to, cost, maximum allocation, and allocation values.
+     * @param anObject the object to compare with this supplier
+     * @return <code>true</code> if the given object represents a Transporter
+     *         equivalent to this transporter, <code>false</code> otherwise
+     */
+    @Override
+    public boolean equals(final Object anObject) {
+        if ((anObject == null)
+                || (this.getClass() != anObject.getClass())) {
+            return false;
+        }
+        Transporter other = ((Transporter) anObject);
+        return (((this.name != null) && this.name.equals(other.name))
+                && ((this.from == null && other.from == null)
+                    || ((this.from != null) && this.from.equals(other.from)))
+                && ((this.to == null && other.to == null)
+                    || ((this.to != null) && this.to.equals(other.to)))
+                && this.costPerUnit == other.costPerUnit
+                && this.maxCapacity == other.maxCapacity
+                && this.allocation == other.allocation);
+    }
+
+    /**
+     * Returns a hash code value for this transporter.
+     * @return hash code value for this transporter
+     */
+    @Override
+    public int hashCode() {
+        if (name == null) return 43;
+        return this.name.hashCode();
+    }
+
+
+    /**
+     * Save this transporter to a file.
+     * @param filename the name of the file in which to save this transporter;
+     *                 if null, uses default file name
+     * @return <code>true</code> if successful save;
+     *         <code>false</code> otherwise
+     * @throws java.io.IOException if unexpected IO error
+     */
+    public final boolean save(final String filename) throws java.io.IOException {
+        boolean success = true;
+        String transporterFileName = filename;
+        if (transporterFileName == null) {
+            transporterFileName = Transporter.SERIAL_FILENAME;
+        }
+        // Serialize the transporter.
+        try {
+            OutputStream file = new FileOutputStream(transporterFileName);
+            OutputStream buffer = new BufferedOutputStream(file);
+            ObjectOutput output = new ObjectOutputStream(buffer);
+            try {
+                output.writeObject(this);
+            } finally { output.close(); }
+        } catch (IOException ex) {
+            System.err.println("Unsuccessful save. " + ex);
+            throw ex;
+        }
+
+        // Attempt to deserialize the transporter as verification.
+        try {
+            InputStream file = new FileInputStream(transporterFileName);
+            InputStream buffer = new BufferedInputStream(file);
+            ObjectInput input = new ObjectInputStream(buffer);
+            try {
+                @SuppressWarnings("unchecked") // Accommodate type erasure.
+                Transporter restored = (Transporter) input.readObject();
+                // Simple check that deserialized data matches original.
+                if (!this.toString().equals(restored.toString())) {
+                    System.err.println("[1] State restore did not match save!");
+                    success = false;
+                }
+                if (!this.equals(restored)) {
+                    System.err.println("[2] State restore did not match save!");
+                    success = false;
+                }
+            } finally { input.close(); }
+        } catch (ClassNotFoundException ex) {
+            System.err.println(
+                "Unsuccessful deserialization: Class not found. " + ex);
+            success = false;
+        } catch (IOException ex) {
+            System.err.println("Unsuccessful deserialization: " + ex);
+            success = false;
+        }
+        return success;
+    }
+
+    /**
+     * Restore this transporter from a file.
+     * <br /><em>Postconditions:</em>
+     * <blockquote>If successful, previous contents of this transporter have
+     * been replaced by the contents of the file.
+     * If unsuccessful, content of the transporter is unchanged.</blockquote>
+     * @param filename the name of the file from which to restore this transporter;
+     *                 if null, uses default file name
+     * @return <code>true</code> if successful restore;
+     *         <code>false</code> otherwise
+     * @throws java.io.IOException if unexpected IO error
+     */
+    public final boolean restore(final String filename) throws
+    java.io.IOException {
+        boolean success = false;
+        String transporterFileName = filename;
+        if (transporterFileName == null) {
+            transporterFileName = Transporter.SERIAL_FILENAME;
+        }
+        Transporter restored = null;
+        try {
+            InputStream file = new FileInputStream(transporterFileName);
+            InputStream buffer = new BufferedInputStream(file);
+            ObjectInput input = new ObjectInputStream(buffer);
+            try {
+                @SuppressWarnings("unchecked") // Accommodate type erasure.
+                
+                Transporter retrieved = (Transporter) input.readObject();
+                restored = retrieved;
+            } finally {
+                input.close();
+                success = true;
+            }
+        } catch (ClassNotFoundException ex) {
+            System.err.println(
+                "Unsuccessful deserialization: Class not found. " + ex);
+            success = false;
+        } catch (IOException ex) {
+            System.err.println("Unsuccessful deserialization: " + ex);
+            throw ex;
+        }
+        if (restored == null) {
+            System.err.println(
+                "Unsuccessful deserialization: restored == null");
+            success = false;
+        } else {
+            this.name = restored.name;
+            this.from = restored.from;
+            this.to = restored.to;
+            this.costPerUnit = restored.costPerUnit;
+            this.maxCapacity = restored.maxCapacity;
+            this.allocation = restored.allocation;
+        }
+        return success;
     }
 }
